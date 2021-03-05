53c9e51f4f639d6eda1ecf8627f469c1e9fd1435
Jennifer Kramer <jenkrame@gmail.com>
2021-03-04 23:49:48 -0500

    Messing with certificates.

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ server.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, function (req, res) {
+    res.writeHead(200);
+    res.end("hello world\n");
+}).listen(8000);
+
 server.listen(port, () => {
     console.log('Listening  on: ' + port );
 });
