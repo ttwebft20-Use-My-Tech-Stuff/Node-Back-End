e7f8149c1fd959719f3f5c624db62b8c8d68407d
Jennifer Kramer <jenkrame@gmail.com>
2021-03-06 15:54:38 -0500

    Completed tests.

--- api/tech-items/tech-items-router.js
+++ api/tech-items/tech-items-router.js
@@ -4,36 +4,33 @@ const TechItems = require("./tech-items-model.js");
 
 const { validateItemId, validateItem, validateOwnerUsername } = require("./tech-items-middleware.js");
 
-// GET - Get array of all tech items - WORKING!
+// GET - Get array of all tech items
 router.get("/", async (req, res, next) => {
     try {
         const data = await TechItems.find();
 
-        // dev-prod Working!
         return res.status(200).json(data);
     } catch(error) {
         next(error);
     }
 });
 
-// GET - Get tech item by ID - WORKING!
+// GET - Get tech item by ID
 router.get("/:id", validateItemId, async (req, res, next) => {
     try {
         const data = await TechItems.findBy({ id: req.params.id });
 
-        // dev-prod Working!
         return res.status(200).json(data);
     } catch(error) {
         next(error);
     }
 });
 
-// POST - Add a tech item - WORKING!
+// POST - Add a tech item
 router.post("/", validateItem, validateOwnerUsername, async (req, res, next) => {
     try {
         const data = await TechItems.add(req.body);
 
-        // dev-prod Working!
         return res.status(201).json(data);
     } catch(error) {
         next(error);
@@ -45,7 +42,6 @@ router.put("/:id", validateItemId, validateItem, validateOwnerUsername, async (r
     try {
         const data = await TechItems.update(req.body, req.params.id);
 
-        // dev Working!
         return res.status(200).json(data);
     } catch(error) {
         next(error);
@@ -55,12 +51,9 @@ router.put("/:id", validateItemId, validateItem, validateOwnerUsername, async (r
 // DELETE - Delete a tech item by ID
 // eslint-disable-next-line no-unused-vars
 router.delete("/:id", validateItemId, validateOwnerUsername, async (req, res, next) => {
-    const itemId = req.params.id;
+    TechItems.remove(req.params.id);
 
-    TechItems.remove(itemId);
-
-    // dev Working!
-    return res.status(200).json(`The tech item with ID: ${itemId} was removed.`);
+    return res.status(200).json(`The tech item with ID: ${req.params.id} was removed.`);
 });
 
 module.exports = router;
